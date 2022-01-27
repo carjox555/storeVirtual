61bbcba89b8f0ea24c29f1673fd1b6bbf631c90f
carjox <carlosbmestra@gmail.com>
2022-01-26 23:13:12 -0500

    cree templates

--- ProjectwebApp/views.py
+++ ProjectwebApp/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def home (request):
+    return render(request,"ProjectwebApp/home.html")
+
+def servicios (request):
+    return render(request,"ProjectwebApp/servicios.html")
+
+def tienda (request):
+    return render(request,"ProjectwebApp/tienda.html")
+
+def blog (request):
+    return render(request,"ProjectwebApp/blog.html")
+
+def contacto (request):
+    return render(request,"ProjectwebApp/contacto.html")
