660a3a72061baad3c0187769824bcacbf843ff36
carjox <carlosbmestra@gmail.com>
2022-02-03 19:33:35 -0500

    se crea servicis en la web

--- servicios/admin.py
+++ servicios/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here. registramos nuestra app
+from .models import Servicio
+
+class ServicioAdmin(admin.ModelAdmin):
+    readonly_fields=('created','updated')
+
+admin.site.register(Servicio,ServicioAdmin)
