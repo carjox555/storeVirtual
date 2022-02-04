12c0a361f2f502312f341e7494e41973bc44656a
carjox <carlosbmestra@gmail.com>
2022-02-03 21:35:04 -0500

    modificado servicios

--- servicios/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here. registramos nuestra app
-from .models import Servicio
-
-class ServicioAdmin(admin.ModelAdmin):
-    readonly_fields=('created','updated')
-
-admin.site.register(Servicio,ServicioAdmin)
